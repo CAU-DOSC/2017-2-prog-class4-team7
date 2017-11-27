de1fb6318b2929bc43fe3156f518e68afe69064b
qqdtf98 <dltjdals98@naver.com>
2017-11-27 19:18:27 +0900

    initial commit

---
+++ file1.c
@@ -0,0 +1,40 @@
+Link createNode(int value)
+{
+	Link cur;
+	cur = (Link)malloc(sizeof(Node));
+	cur->value = (int *)malloc(sizeof(int));
+	cur->value = value;
+	cur->next = NULL;
+	return cur;
+}
+
+Link append(Link head, Link cur)
+{
+	Link nextNode = head;
+	if (head == NULL)
+	{
+		head = cur;
+		return head;
+	}
+	while (nextNode->next != NULL)
+	{
+		nextNode = nextNode->next;
+	}
+	nextNode->next = cur;
+
+	return head;
+}
+
+Link printValue(Link head, int i)
+{
+	Link cur = head;
+	int n = 0;
+	while (n < i)
+	{
+		cur = cur->next;
+		n++;
+	}
+	printf("%d ", cur->value);
+
+	return head;
+}
