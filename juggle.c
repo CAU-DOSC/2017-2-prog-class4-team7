6f608039edead07d5968761b13ddd34ab1838690
huuuun <zaqxsw1397@naver.com>
2017-12-05 15:13:43 +0900

    hi

--- juggle.c
+++ juggle.c
@@ -6,7 +6,37 @@ void juggle(char line[], int n, int d) {
 
 	if (d >= 0) {
 		if (d >= n) {
-			d = n%d;
+			d %= n;
+		}
+		for (int i = 0; i < d; i++) {
+			temp[i] = line[n - d + i];
+		}
+		for (int k = 0; k<d; k++) {
+			if (n%d == k) {
+				for (int j = 0; j<k; j++) {
+					for (int i = (int)(n / d), j = 1; j <= i; i--) {
+						line[d*i + cnt_n] = line[d*(i - 1) + cnt_n];
+					}
+					cnt_n++;
+				}
+
+				for (int j = k; j<d; j++) {
+					for (int i = (int)(n / d), j = 1; j <= i; i--) {
+						line[d*(i - 1) + cnt_n] = line[d*(i - 2) + cnt_n];
+					}
+					cnt_n++;
+				}
+			}
+		}
+		for (int i = 0; i < d; i++) {
+			line[i] = temp[i];
+		}
+	}
+
+	else {
+		d = -d;
+		if (d >= n) {
+			d %= n;
 		}
 		for (int i = 0; i < d; i++) {
 			temp[i] = line[i];
@@ -14,7 +44,7 @@ void juggle(char line[], int n, int d) {
 		for (int k = 0; k<d; k++) {
 			if (n%d == k) {
 				for (int j = 0; j<k; j++) {
-					for (int i = 1; i<=(int)(n / d); i++) {
+					for (int i = 1; i <= (int)(n / d); i++) {
 						line[d*(i - 1) + cnt_p] = line[d*i + cnt_p];
 					}
 					cnt_p++;
@@ -30,36 +60,6 @@ void juggle(char line[], int n, int d) {
 		}
 		for (int i = 0; i < d; i++) {
 			line[n - d + i] = temp[i];
-		}
-	}
-
-	else {
-		d = -d;
-		if (d >= n) {
-			d = n%d;
-		}
-		for (int i = 0; i < d; i++) {
-			temp[i] = line[n - d + i];
-		}
-		for (int k = 0; k<d; k++) {
-			if (n%d == k) {
-				for (int j = 0; j<k; j++) {
-					for (int i = (int)(n / d), j = 1; j <= i; i--) {
-						line[d*i + cnt_n] = line[d*(i-1) + cnt_n];
-					}
-					cnt_n++;
-				}
-
-				for (int j = k; j<d; j++) {
-					for (int i = (int)(n / d), j = 1; j <= i; i--) {
-						line[d*(i - 1) + cnt_n] = line[d*(i-2) + cnt_n];
-					}
-					cnt_n++;
-				}
-			}
-		}
-		for (int i = 0; i < d; i++) {
-			line[i] = temp[i];
 		}
 	}
 }
